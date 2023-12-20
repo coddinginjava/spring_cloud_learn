7940fdb1b09953aecbfaf73f38a1fd58bb6c96f9
Sai Prakash <coddinginjava@gmail.com>
2023-12-20 17:26:32 +0530

    Cloud config done

---
+++ UsersMS/src/test/java/com/sai/UsersMS/UsersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.UsersMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
